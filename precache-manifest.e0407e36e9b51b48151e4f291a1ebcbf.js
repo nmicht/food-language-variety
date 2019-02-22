74e3e6bbda0fbab074bb6f491cb21b11dea8ff8a
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-02-21 22:05:24 -0500

    Updates

---
+++ precache-manifest.e0407e36e9b51b48151e4f291a1ebcbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf46237e023dc7cf7c5c",
+    "url": "/food-language-variety/static/js/runtime~main.bf46237e.js"
+  },
+  {
+    "revision": "a1b665534ac043381b93",
+    "url": "/food-language-variety/static/js/main.a1b66553.chunk.js"
+  },
+  {
+    "revision": "f6159989f24f48a0a69e",
+    "url": "/food-language-variety/static/js/2.f6159989.chunk.js"
+  },
+  {
+    "revision": "a1b665534ac043381b93",
+    "url": "/food-language-variety/static/css/main.fa8a65a9.chunk.css"
+  },
+  {
+    "revision": "e2a6f502c20b4f0764a94570b61e98ed",
+    "url": "/food-language-variety/index.html"
+  }
+];
