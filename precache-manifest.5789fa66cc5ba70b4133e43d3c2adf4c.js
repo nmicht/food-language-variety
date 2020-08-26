1e0b355938cbf01356868e3c82d5dd24807e6a6c
Mihelle Torres <lic.nancy.torres@gmail.com>
2020-08-26 21:25:18 +0200

    Updates

---
+++ precache-manifest.5789fa66cc5ba70b4133e43d3c2adf4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d024d7d2d404250829fc78f2e124a8",
+    "url": "/food-language-variety/index.html"
+  },
+  {
+    "revision": "7d3144b6985296c4fdc5",
+    "url": "/food-language-variety/static/css/main.9f39ccd6.chunk.css"
+  },
+  {
+    "revision": "b089ec7f13f654972e98",
+    "url": "/food-language-variety/static/js/2.35f3dac6.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/food-language-variety/static/js/2.35f3dac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3144b6985296c4fdc5",
+    "url": "/food-language-variety/static/js/main.04b71b6f.chunk.js"
+  },
+  {
+    "revision": "09423eb4cb35d1ece1b1",
+    "url": "/food-language-variety/static/js/runtime-main.bac07ec0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-language-variety/precache-manifest.576bcdfe15e6dd14ca9e686c8199a0b9.js"
+  "/food-language-variety/precache-manifest.5789fa66cc5ba70b4133e43d3c2adf4c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/food-language-variety/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/food-language-variety/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
